6d3344633a50a2db4b2ce6b7f5f736912a87de19
FoxArchon <Battlefront97@msn.com>
2025-09-22 18:19:29 -0600

    Next update
    Added som.e psudocode type stuff

--- GDD 3400 Lab 01/Assets/GDD 3400 - Herding Sheep/Scripts/Dog.cs
+++ GDD 3400 Lab 01/Assets/GDD 3400 - Herding Sheep/Scripts/Dog.cs
@@ -4,6 +4,8 @@ namespace GDD3400.Project01
 {
     public class Dog : MonoBehaviour
     {
+
+        //private RigidBody _rb;
         
         private bool _isActive = true;
         public bool IsActive 
@@ -45,11 +47,84 @@ namespace GDD3400.Project01
 
         private void Perception()
         {
+
+            //this code is similar to the sheep perception except the threat possibilites are not needed
+
+           // _friendTargets.Clear();
+           // _safeZoneTarget = null;
+
+            // Collect all target colliders within the sight radius
+            //int t = Physics.OverlapSphereNonAlloc(transform.position, _sightRadius, _tmpTargets, _targetsLayer);
+            //for (int i = 0; i < t; i++)
+            //{
+               // var c = _tmpTargets[i];
+                //if (c==null || c.gameObject == gameObject) continue;
+
+                // Store the friends and safe zone targets
+                //switch (c.tag)
+                //{
+                   // case _friendTag:
+                        //_friendTargets.Add(c);
+                        //break;
+                   
+                    //case _safeZoneTag:
+                       // _safeZoneTarget = c;
+                       // break;
+                //}
+            //}
             
         }
 
         private void DecisionMaking()
         {
+
+
+            
+            //if sheep are seen, go near to the sheep
+
+            //This is simialr to the sheep behavior that goes toward the do if it is a freind except speed is defaukt for the dog.
+
+
+            // foreach (var friend in _friendTargets)
+           // {
+               // if (friend.GetComponentInParent<Dog>() != null)
+               // {
+                    //_target = friend.transform.position;
+                   // return;
+               // }
+            //}
+
+
+            //if certain distance from sheep go back toward safezone at a walk 
+
+            //check if dog is close enough
+           // if(var friend.position - var dog.postion < 5 )
+            //{
+
+                //walk toward safezone
+
+                //_target = _safeZoneTarget;
+
+
+
+           // }
+
+            
+            //if no sheep seen that are not in safe zone, go wander
+
+            //if(_friendTargets = null or freindTargets == InSafeZone)
+
+            //{
+
+                // go back to wander behavior
+
+
+            //}
+
+            //if no sheep within sight, rotate 360 degrees
+
+
+
 
         }
 
@@ -60,6 +135,8 @@ namespace GDD3400.Project01
         private void FixedUpdate()
         {
             if (!_isActive) return;
+
+            
             
         }
     }
